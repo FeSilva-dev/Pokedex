40443b022ad06ef3530ae58fa498e8ae6f0ab52a
FeSilva-dev <feliper.silva011@gmail.com>
2020-11-08 15:49:16 -0300

    some changes in files

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,11 @@
 import react from 'react'
 import { Route, BrowserRouter } from 'react-router-dom'
-
-import Index from './components/index'
 import Pokedex from './components/page'
 
 const Routes = () => {
     return(
         <BrowserRouter>
-            <Route component={Index} path="/" exact/>
-            <Route component={Pokedex} path="/pokedex" />
+            <Route component={Pokedex} path="/" exact/>
         </BrowserRouter>
     )
 }
